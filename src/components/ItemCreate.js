d36db8627bdd8a15690055a9f3a19857e1d20259
kz-gonzalo-curbelo <57901933+kz-gonzalo-curbelo@users.noreply.github.com>
2019-11-29 16:06:40 -0300

    Update ItemCreate.js

--- src/components/ItemCreate.js
+++ src/components/ItemCreate.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Create, SimpleForm, TextInput, DateInput } from "react-admin";
+import { Create, SimpleForm, TextInput, NumberInput } from "react-admin";
 
 export const ItemCreate = props => (
   <Create {...props}>
     <SimpleForm>
-      <TextInput source="Item Name" />
-      <TextInput source="Bar code" />
-      <DateInput source="Price" />
+      <TextInput source="name" />
+      <TextInput source="bar_code" />
+      <NumberInput source="price" />
     </SimpleForm>
   </Create>
 );
